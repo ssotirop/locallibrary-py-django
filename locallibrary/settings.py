c10805def41fdf2aabd67b633fdf5918e119991e
ssotirop <sotiris.sotiropoulos@gmail.com>
2019-05-24 12:01:52 +0100

    Added files and changes required for deployment to heroku

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ge5*5f$af+v-ly6qbxa-m=)3*e4q1b_6z8wf&i=apfh1o7fme-'
+#SECRET_KEY = 'ge5*5f$af+v-ly6qbxa-m=)3*e4q1b_6z8wf&i=apfh1o7fme-'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ge5*5f$af+v-ly6qbxa-m=)3*e4q1b_6z8wf&i=apfh1o7fme-')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['192.168.56.10']
+# Set hosts to allow any app on Heroku and the local testing URL
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -37,16 +41,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
     # Third party
     'bootstrap4',
-
-    # My apps
+    # My application(s)
     'catalog.apps.CatalogConfig',
 ]
 
+# Added WhiteNoise to serve static files in production
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,16 +127,33 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Redirect to home URL after login
+# (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+# The absolute path to the directory where collectstatic will collect
+# static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+
+# OPTIONAL: Reduce the size of the static files when they are served
+# (this is more efficient)
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Settings for django-bootstrap4
 BOOTSTRAP4 = {
     'include_jquery': True,
 }
-
-# Redirect to home URL after login
-# (Default redirects to /accounts/profile/)
-LOGIN_REDIRECT_URL = '/'
